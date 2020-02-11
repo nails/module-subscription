c9b7f16a1aa6bc6489f04ac51a34564d5bf02274
Pablo de la Peña <p@shedcollective.org>
2020-02-11 17:21:28 +0000

    Adds generic SubscriptionException

---
+++ src/Exception/SubscriptionException.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Subscription exception
+ *
+ * @package     Nails
+ * @subpackage  module-subscription
+ * @category    Exception
+ * @author      Nails Dev Team
+ * @link        https://docs.nailsapp.co.uk/modules/subscription
+ */
+
+namespace Nails\Subscription\Exception;
+
+use Nails\Common\Exception\NailsException;
+
+/**
+ * Class SubscriptionException
+ *
+ * @package Nails\Subscription\Exception
+ */
+class SubscriptionException extends NailsException
+{
+}
